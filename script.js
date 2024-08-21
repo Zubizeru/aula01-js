f9436aee1c332b605cabc4c70bf24880395d646f
Zubizeru <jose.ehmendes@senacsp.edu.br>
2024-08-21 20:20:48 -0300

    feat: Nota e média do Primeiro e Segundo Aluno js

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
-//Exibir no terminal
-console.log("Olá Mundo");
-//Exibir no navegador
-document.write("Olá Navegador");
-//Um alerta
-alert("Alerta de Confirmação!");
+/*
+Faça um algoritmo onde o usuário informe o nome e 4 notas de dois alunos e mostre a média desses 2 alunos.
+*/
 
-//string
+//Capturar o nome do Primeiro aluno
+var nome1 = window.prompt("Informe o seu nome:");
 
-//Capturar o nome do Aluno
-var nome = window.prompt("Informe o seu nome:");
-
-//Guardar a primeira nota
+//Guardar notas do Primeiro aluno
 var nota1 = window.prompt("Digite a nota 1:");
-
-//Converte a nota1 de texto para decimal
 nota1 = parseFloat(nota1);
-
-//Guardar a segunda nota
 var nota2 = window.prompt("Digite a nota 2:");
-
-//Converte a nota2 de texto para decimal
 nota2 = parseFloat(nota2);
+var nota3 = window.prompt("Digite a nota 3:");
+nota3 = parseFloat(nota3);
+var nota4 = window.prompt("Digite a nota 4:");
+nota4 = parseFloat(nota4);
 
-//Calcule a média das duas notas
-var media = (nota1 + nota2)/2;
+//Calcule a média das duas notas do Primeiro aluno
+var media1 = (nota1 + nota2 + nota3 + nota4)/4;
 
-//Gera a mensagem de exibição do nome e da média do aluno 
-var mensagem = nome + ", você obteve a média: " + media;
+//Capturar o nome do Segundo Aluno
+var nome2 = window.prompt("Informe o seu nome:");
+
+//Guardar notas do Segundo aluno
+var nota10 = window.prompt("Digite a nota 1:");
+nota10 = parseFloat(nota10);
+var nota20 = window.prompt("Digite a nota 2:");
+nota20 = parseFloat(nota20);
+var nota30 = window.prompt("Digite a nota 3:");
+nota30 = parseFloat(nota30);
+var nota40 = window.prompt("Digite a nota 4:");
+nota40 = parseFloat(nota40);
+
+//Calcule a média das duas notas do Segundo Aluno
+var media2 = (nota10 + nota20 + nota30 + nota40)/4;
+
+
+//Gera a mensagem de exibição do nome e da média do Primeiro e Segundo aluno 
+var mensagem = nome1 + ", você obteve a média: " + media1 + " e " + nome2 + ", você obteve a média: " + media2 ;
 window.alert(mensagem);
