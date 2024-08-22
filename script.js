f4d9f0f541eedeabe67f43865924c9eebb821696
Zubizeru <jose.ehmendes@senacsp.edu.br>
2024-08-21 21:28:15 -0300

    feat: correção da nota e média do Primeiro e Segundo Aluno js

--- script.js
+++ script.js
@@ -1,40 +1,31 @@
 /*
-Faça um algoritmo onde o usuário informe o nome e 4 notas de dois alunos e mostre a média desses 2 alunos.
+Faça um algoritmo onde o usuário informe o nome
+e 4 notas de dois alunos e mostre as
+médias desses 2 alunos.
 */
-
-//Capturar o nome do Primeiro aluno
-var nome1 = window.prompt("Informe o seu nome:");
-
-//Guardar notas do Primeiro aluno
-var nota1 = window.prompt("Digite a nota 1:");
-nota1 = parseFloat(nota1);
-var nota2 = window.prompt("Digite a nota 2:");
-nota2 = parseFloat(nota2);
-var nota3 = window.prompt("Digite a nota 3:");
-nota3 = parseFloat(nota3);
-var nota4 = window.prompt("Digite a nota 4:");
-nota4 = parseFloat(nota4);
-
-//Calcule a média das duas notas do Primeiro aluno
-var media1 = (nota1 + nota2 + nota3 + nota4)/4;
-
-//Capturar o nome do Segundo Aluno
-var nome2 = window.prompt("Informe o seu nome:");
-
-//Guardar notas do Segundo aluno
-var nota10 = window.prompt("Digite a nota 1:");
-nota10 = parseFloat(nota10);
-var nota20 = window.prompt("Digite a nota 2:");
-nota20 = parseFloat(nota20);
-var nota30 = window.prompt("Digite a nota 3:");
-nota30 = parseFloat(nota30);
-var nota40 = window.prompt("Digite a nota 4:");
-nota40 = parseFloat(nota40);
-
-//Calcule a média das duas notas do Segundo Aluno
-var media2 = (nota10 + nota20 + nota30 + nota40)/4;
-
-
-//Gera a mensagem de exibição do nome e da média do Primeiro e Segundo aluno 
-var mensagem = nome1 + ", você obteve a média: " + media1 + " e " + nome2 + ", você obteve a média: " + media2 ;
-window.alert(mensagem);
+var nome1, nome2 = "";
+var nota1, nota2, nota3, nota4,
+    nota5, nota6, nota7, nota8 = 0;
+const quantidadeProvas = 4;
+ 
+nome1 = prompt("Digite o Nome do Aluno 1:");
+nota1 = parseFloat(prompt("Digite a Nota 1 do Aluno 1: "));
+nota2 = parseFloat(prompt("Digite a Nota 2 do Aluno 1: "));
+nota3 = parseFloat(prompt("Digite a Nota 3 do Aluno 1: "));
+nota4 = parseFloat(prompt("Digite a Nota 4 do Aluno 1: "));
+ 
+nome2 = prompt("Digite o Nome do Aluno 2:");
+nota5 = parseFloat(prompt("Digite a Nota 1 do Aluno 2: "));
+nota6 = parseFloat(prompt("Digite a Nota 2 do Aluno 2: "));
+nota7 = parseFloat(prompt("Digite a Nota 3 do Aluno 2: "));
+nota8 = parseFloat(prompt("Digite a Nota 4 do Aluno 2: "));
+ 
+var media1, media2 = 0;
+media1 = (nota1 + nota2 + nota3 + nota4) / quantidadeProvas;
+media2 = (nota5 + nota6 + nota7 + nota8) / quantidadeProvas;
+ 
+var mensagem = `
+${nome1}, sua média é ${media1}, parabéns!  
+${nome2}, sua média é ${media2}, parabéns!`;
+ 
+alert(mensagem);
